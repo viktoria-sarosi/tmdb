ff48c79954c07b7df5f26ed0dba7615c5f963617
viktoria-sarosi <domanchana@gmail.com>
2020-05-03 12:24:48 +0200

    created controllers package to test connection

---
+++ src/main/java/com/tmdbproject/TmdbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tmdbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmdbProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmdbproject/TmdbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmdbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
