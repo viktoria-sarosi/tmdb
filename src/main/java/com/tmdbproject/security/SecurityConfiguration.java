66b2183abf6e840330ce028d32c581be2caac28f
viktoria-sarosi <domanchana@gmail.com>
2020-05-04 16:33:34 +0200

    configure(authorization)

--- src/main/java/com/tmdbproject/security/SecurityConfiguration.java
+++ src/main/java/com/tmdbproject/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.tmdbproject.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,11 +16,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user")
                 .password("password")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("otherUser")
+                .password("otherPassword")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
+
+
 }
