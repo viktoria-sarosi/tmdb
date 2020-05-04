024815fbadd6e6df3b06a220f896e94a38eb2a8b
viktoria-sarosi <domanchana@gmail.com>
2020-05-04 15:47:42 +0200

    configure(authentication)

--- src/main/java/com/tmdbproject/controllers/MainController.java
+++ src/main/java/com/tmdbproject/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.tmdbproject.controllers;
 import com.tmdbproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
@@ -13,4 +14,6 @@ public class MainController {
     public MainController(UserService userService) {
         this.userService = userService;
     }
+
+
 }
